3d837159f7318b43162f2a93e13eb0c341825014
vamsi krishna <vamsimedikonda1743@gmail.com>
2025-06-25 09:27:29 +0530

    sysots removed

--- demo/src/main/java/com/demo/DemoApplication.java
+++ demo/src/main/java/com/demo/DemoApplication.java
@@ -8,11 +8,10 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Spring App Stated");
 		
 		SpringApplication.run(DemoApplication.class, args);
 		
-		System.out.println("Spring App Ended");
+	
 	}
 
 }
